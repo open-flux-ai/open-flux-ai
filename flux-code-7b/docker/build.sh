99499c9a671341464facaf81e798ab6419573f08
Chanwit Kaewkasi <chanwit@gmail.com>
2024-02-14 20:01:25 +0700

    change model to magi-codellama

--- flux-code-7b/docker/build.sh
+++ flux-code-7b/docker/build.sh
@@ -2,7 +2,7 @@ source ./VERSION
 
 rm model.gguf || true
 wget -qO model.gguf \
-  https://huggingface.co/TheBloke/deepseek-coder-6.7B-instruct-GGUF/resolve/main/deepseek-coder-6.7b-instruct.Q5_K_M.gguf
+  https://huggingface.co/tsunemoto/Magicoder-S-CL-7B-GGUF/resolve/main/magicoder-s-cl-7b.Q5_K_M.gguf
 
 docker build \
 --build-arg IMAGE=${IMAGE} \
