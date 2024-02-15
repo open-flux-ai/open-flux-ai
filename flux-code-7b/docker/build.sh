b97c7a6bfd10208138d863e5081dc30dead5b343
Chanwit Kaewkasi <chanwit@gmail.com>
2024-02-15 20:13:08 +0700

    add gpu build
    Signed-off-by: Chanwit Kaewkasi <chanwit@gmail.com>

--- flux-code-7b/docker/build.sh
+++ flux-code-7b/docker/build.sh
@@ -4,10 +4,19 @@ rm model.gguf || true
 wget -qO model.gguf \
   https://huggingface.co/tsunemoto/Magicoder-S-CL-7B-GGUF/resolve/main/magicoder-s-cl-7b.Q5_K_M.gguf
 
+# Build CPU (avx2)
 docker build \
---build-arg IMAGE=${IMAGE} \
+--build-arg IMAGE=${IMAGE}-avx2 \
 -t ghcr.io/open-flux-ai/flux-code-7b:${VERSION} .
 
 docker push ghcr.io/open-flux-ai/flux-code-7b:${VERSION}
+
+# Build GPU image
+docker build \
+-f Dockerfile.gpu \
+--build-arg IMAGE=${IMAGE}-gpu \
+-t ghcr.io/open-flux-ai/flux-code-7b:${VERSION}-gpu .
+
+docker push ghcr.io/open-flux-ai/flux-code-7b:${VERSION}-gpu
 
 rm model.gguf || true
